46faa0fa6af7906f01511e5633b381414ec79650
莫离 <327660509@qq.com>
2022-11-13 09:58:05 +0800

    Update Nicegram.js

--- Script/Nicegram.js
+++ Script/Nicegram.js
@@ -1,3 +1,32 @@
+/***********************************
+            
+[rewrite_local]
+# ✅Nicegram
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://github.com/Moli-X/Resources/raw/main/Script/Nicegram.js
+
+
+[mitm]
+hostname= restore-access.indream.app
+***********************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 {
   "data": {
     "premiumAccess": true

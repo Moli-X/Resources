d625f48e5370127ce8be3e2eccff45b422c47629
莫离 <327660509@qq.com>
2022-11-15 16:49:22 +0800

    Update Pornhub.js

--- Script/Pornhub/Pornhub.js
+++ Script/Pornhub/Pornhub.js
@@ -1,3 +1,24 @@
+
+/**************************************
+[rewrite_remote]
+# ✅ PornHub
+^https:\/\/(cn|www)\.pornhub\.com\/_xa\/ads url reject-dict
+^https?:/\/(cn|www)\.pornhub\.com(\/?$|\/([?]|view|video).*$) url script-response-body https://github.com/Moli-X/Resources/raw/main/Script/Pornhub/Pornhub.js
+
+[mitm]
+hostname = *.pornhub.com
+****************************************/
+
+
+
+
+
+
+
+
+
+
+
 var body = $response.body
     .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Moli-X/Resources/main/Script/Pornhub/Pornhub.css" type="text/css">');
 $done({ body });

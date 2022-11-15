7dfddff219cf8fe53c19dd987f24255af581bf98
莫离 <327660509@qq.com>
2022-11-15 16:43:29 +0800

    Create Pornhub.js

---
+++ Script/Pornhub/Pornhub.js
@@ -0,0 +1,3 @@
+var body = $response.body
+    .replace(/<head>/, '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/Moli-X/Resources/main/Script/Pornhub/Pornhub.css" type="text/css">');
+$done({ body });

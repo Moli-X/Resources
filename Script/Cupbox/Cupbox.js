2baa8ef8f7b3030b2f805253e6e5305a32613420
莫离 <327660509@qq.com>
2022-11-16 12:15:31 +0800

    Update Cupbox.js

--- Script/Cupbox/Cupbox.js
+++ Script/Cupbox/Cupbox.js
@@ -1,3 +1,23 @@
+
+/***********************************
+[rewrite_local]
+# 茶杯狐
+^https?:/\/cupfox\.app(\/?$|\/([?]|search).*$) url script-response-body https://github.com/Moli-X/Resources/raw/main/Script/Cupbox/Cupbox.js
+
+[mitm]
+hostname= cupfox.app
+***********************************/
+
+
+
+
+
+
+
+
+
+
+
 var body = $response.body
     .replace(/<head>/, '<head><link rel="stylesheet" href="https://github.com/Moli-X/Resources/raw/main/Script/Cupbox/Cupbox.css" type="text/css">');
 $done({ body });

35c25093e91f7b9749f85540a29d7a668f4185d0
莫离 <327660509@qq.com>
2022-11-18 16:25:13 +0800

    Create PS.js

---
+++ Rewrite/PS.js
@@ -0,0 +1,8 @@
+#  https://t.me/QuantX  
+
+[rewrite_local]
+
+^https:\/\/lcs-mobile-cops\.adobe\.io\/mobile_profile url script-response-body https://github.com/Moli-X/Resources/raw/main/Script/PS.js
+
+[mitm] 
+hostname= lcs-mobile-cops.adobe.io, photos.adobe.io

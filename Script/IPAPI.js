d262b8e4bf22e1b761b759374ba47b6f92d3bb09
莫离 <327660509@qq.com>
2022-12-02 18:58:58 +0800

    Create IPAPI.js

---
+++ Script/IPAPI.js
@@ -0,0 +1,20 @@
+/*
+原链接：https://raw.githubusercontent.com/CenBoMin/QuantumultX/master/IP.js
+*/
+
+
+
+
+if ($response.statusCode != 200) {
+  $done(Null);
+}
+
+var body = $response.body;
+var obj = JSON.parse(body);
+var title = obj['country'];
+var subtitle = obj['city'] + ' ' + obj['isp'];
+var ip = obj['query'];
+var description = "国家" + ":" + obj['country'] + '\n' + "城市" + ":" + obj['city'] + '\n' + "运营商" + ":" + obj['isp'] + '\n' + "数据中心" + ":" + obj['org'];
+
+
+$done({title, subtitle, ip, description});

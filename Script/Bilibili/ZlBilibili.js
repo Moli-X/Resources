bd0fe9797c1dc8ba92903bde0567342d9693ed94
莫离 <327660509@qq.com>
2023-03-14 03:11:53 +0800

    Create ZlBilibili.js

---
+++ Script/Bilibili/ZlBilibili.js
@@ -0,0 +1,33 @@
+[rewrite_local]
+
+# 直连模式
+^https?:\/\/app\.bilibili\.com\/x\/resource\/domain url script-response-body https://github.com/Moli-X/Resources/Scripts/Bilibili/raw/main/ZlBilibili.js
+
+[mitm] 
+
+hostname=api.bilibili.com
+
+***********************************/
+
+
+
+
+const Group = $prefs.valueForKey('BiliArea_Policy') || '港台番剧';
+
+const message = {
+    action: "set_policy_state",
+    content: {[Group]: "direct"}
+};
+$configuration.sendMessage(message).then(resolve => {
+    if (resolve.error) {
+        console.log(resolve.error);
+    }
+    if (resolve.ret) {
+        let output=JSON.stringify(resolve.ret);
+        //console.log(output);
+    }
+    $done();
+}, reject => {
+    // Normally will never happen.
+    $done();
+});

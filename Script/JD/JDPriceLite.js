dddb1de555dbb7e48c1c04f566fabde5703ffc8c
莫离 <327660509@qq.com>
2023-07-02 15:28:37 +0800

    Update and rename JDPrice.js to JDPriceLite.js

--- Script/JD/JDPriceLite.js
+++ Script/JD/JDPriceLite.js
@@ -1,15 +1,18 @@
-/*
+/*******************************************
+# 京东比价弹窗版
 Loon/Surge
+原链接:https://raw.githubusercontent.com/Tartarus2014/Script/master/jd_price_lite.js
 [Script]
-http-response ^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) requires-body=1,script-path=https://raw.githubusercontent.com/Tartarus2014/Script/master/jd_price_lite.js, tag=京东比价
+http-response ^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) requires-body=1,script-path=https://github.com/Moli-X/Resources/raw/main/Script/JD/JDPricLite.js, tag=京东比价弹窗版
 [MITM]
 hostname = api.m.jd.com
 
 QX:
-^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) url script-response-body https://github.com/Moli-X/Resources/raw/main/Script/JD/JDPrice.js
+^https?://api\.m\.jd\.com/(client\.action|api)\?functionId=(wareBusiness|serverConfig|basicConfig|lite_wareBusiness|pingou_item) url script-response-body https://github.com/Moli-X/Resources/raw/main/Script/JD/JDPricLite.js
 
 Boxjs订阅：https://raw.githubusercontent.com/githubdulong/Script/master/boxjs.json
-*/
+**************************************/
+
 
 const path1 = "serverConfig";
 const path2 = "wareBusiness";

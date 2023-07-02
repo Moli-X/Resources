110129a04d815a3f64fb99fd19ef1fb8a84472e3
莫离 <327660509@qq.com>
2023-07-02 16:08:11 +0800

    Update TikTok.js

--- Script/TikTok.js
+++ Script/TikTok.js
@@ -1,5 +1,3 @@
-# 原链接https://raw.githubusercontent.com/Keywos/rule/main/loon/tk.js
-
 let keyus={日本: "JP", 韩国: "KR", 英国:"UK", 美国:"US", 台湾:"TW", 香港:"HK", 新加坡:"SG", 法国:"FR", 马来西亚:"MY", 菲律宾:"PH", 泰国:"TH", 手动输入:"inkey"},
 lk = $persistentStore.read("解锁地区"),loc = keyus[lk] || "KR",url = $request.url;
 if(loc == "inkey"){
